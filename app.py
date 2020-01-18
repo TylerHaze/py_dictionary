d6800f9805c3953e5b043e9840be53be45bc4b8b
Tyler Hazleton <tylerhazleton@gmail.com>
2020-01-18 13:05:27 -0800

    handling for proper nouns and acronyms added

--- app.py
+++ app.py
@@ -7,10 +7,20 @@ def getDef(word):
     word = word.lower()
     if word in data:
         return data[word]
-    elif len(get_close_matches(word, data.keys(), cutoff=0.8)) > 0:
+    elif word.title() in data:
+        return data[word.title()]
+    elif word.upper() in data:
+        return data[word.upper()]
+    elif len(get_close_matches(word, data.keys(), cutoff=0.9)) > 0:
         con = input("Did you mean %s?[y/n] " % get_close_matches(word, data.keys(), cutoff=0.8)[0])
         if con == 'y':
             return data[get_close_matches(word, data.keys(), cutoff=0.8)[0]]
+        else:
+            return "Word not found. Goodbye." 
+    elif len(get_close_matches(word.title(), data.keys(), cutoff=0.8)) > 0:
+        con = input("Did you mean %s?[y/n] " % get_close_matches(word.title(), data.keys(), cutoff=0.8)[0])
+        if con == 'y':
+            return data[get_close_matches(word.title(), data.keys(), cutoff=0.8)[0]]
         else:
             return "Word not found. Goodbye." 
     else:
