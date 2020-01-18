946f2cc481ff5aca9212d823bdb0f73634cf714f
Tyler Hazleton <tylerhazleton@gmail.com>
2020-01-17 22:14:22 -0800

    created error handling and better print formatting

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import json
+from difflib import get_close_matches
 
 data = json.load(open("data.json"))
 
@@ -6,10 +7,20 @@ def getDef(word):
     word = word.lower()
     if word in data:
         return data[word]
+    elif len(get_close_matches(word, data.keys(), cutoff=0.8)) > 0:
+        con = input("Did you mean %s?[y/n] " % get_close_matches(word, data.keys(), cutoff=0.8)[0])
+        if con == 'y':
+            return data[get_close_matches(word, data.keys(), cutoff=0.8)[0]]
+        else:
+            return "Word not found. Goodbye." 
     else:
-        return "Word not found."
+        return "Word not found. Check your input."
 
 searchDef = input("What word would you like to define: ")
 definition = getDef(searchDef)
 
-print(definition)
+if type(definition) is list:
+    for line in definition:
+        print(line)
+else:
+    print(definition)
